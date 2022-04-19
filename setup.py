a3bb595b4d802719274ef14a5c8fb9a1500077e9
Clayton <cwseitz@indiana.edu>
2022-04-19 19:55:33 -0400

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup, Extension
 import numpy
 
 def main():
-
+    
+    """
     setup(name="arwn",
           version="1.0.0",
           description="Automated gene Regulatory netWork aNalysis",
@@ -11,6 +12,15 @@ def main():
           ext_modules=[Extension("core", ["arwn/core/arwn_core.c"],
                        include_dirs = [numpy.get_include()],
                        library_dirs = ['/usr/lib/x86_64-linux-gnu'])])
+
+    """
+
+    setup(name="arwn",
+          version="1.0.0",
+          description="Automated gene Regulatory netWork aNalysis",
+          author="Clayton Seitz",
+          author_email="cwseitz@iu.edu")
+
 
 
 if __name__ == "__main__":
