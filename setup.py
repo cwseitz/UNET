97f857012c2dd1e26de24e9191e78617a7f161b5
Clayton <cwseitz@indiana.edu>
2022-04-19 15:29:48 -0400

    update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def main():
           description="Automated gene Regulatory netWork aNalysis",
           author="Clayton Seitz",
           author_email="cwseitz@iu.edu",
-          ext_modules=[Extension("arwn_core", ["arwn/core/arwn_core.c"],
+          ext_modules=[Extension("core", ["arwn/core/arwn_core.c"],
                        include_dirs = [numpy.get_include()],
                        library_dirs = ['/usr/lib/x86_64-linux-gnu'])])
 
