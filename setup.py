3099ed2039395aff4fbaf0faa3f3e8f41e911495
Clayton <cwseitz@indiana.edu>
2022-04-19 15:25:59 -0400

    merge core

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from setuptools import setup, find_packages
+from distutils.core import setup, Extension
+import numpy
 
-setup = setup(
-    name='arwn',
-    description='PyTorch implementations',
-    version='1.0',
-    packages=find_packages(),
-)
+def main():
+
+    setup(name="arwn",
+          version="1.0.0",
+          description="Automated gene Regulatory netWork aNalysis",
+          author="Clayton Seitz",
+          author_email="cwseitz@iu.edu",
+          ext_modules=[Extension("arwn_core", ["arwn/core/arwn_core.c"],
+                       include_dirs = [numpy.get_include()],
+                       library_dirs = ['/usr/lib/x86_64-linux-gnu'])])
+
+
+if __name__ == "__main__":
+    main()
