712c2f110cd8840df745f0f1fc7df5b3b9434397
Clayton <cwseitz@indiana.edu>
2022-04-19 20:04:11 -0400

    core -> backend

--- setup.py
+++ setup.py
@@ -3,23 +3,14 @@ import numpy
 
 def main():
     
-    """
     setup(name="arwn",
           version="1.0.0",
           description="Automated gene Regulatory netWork aNalysis",
           author="Clayton Seitz",
           author_email="cwseitz@iu.edu",
-          ext_modules=[Extension("core", ["arwn/core/arwn_core.c"],
+          ext_modules=[Extension("backend", ["arwn/backend/backend.c"],
                        include_dirs = [numpy.get_include()],
                        library_dirs = ['/usr/lib/x86_64-linux-gnu'])])
-
-    """
-
-    setup(name="arwn",
-          version="1.0.0",
-          description="Automated gene Regulatory netWork aNalysis",
-          author="Clayton Seitz",
-          author_email="cwseitz@iu.edu")
 
 
 
